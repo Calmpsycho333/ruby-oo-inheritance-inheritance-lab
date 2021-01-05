1ad987f9ba549b67620c7e014b9405a23b70e7ee
calmpsycho333 <wrathking75@yahoo.com>
2021-01-05 08:55:12 -0500

    commit tree 18b2c816b3101aae11704a3398793ed48651b51a

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require'pry'
+class Student < User 
+attr_accessor :first_name, :last_name
+
+def initialize
+    @knowledge = []
+end
+
+def learn(string_of_knowledge)
+    @string_of_knowledge = string_of_knowledge
+     @knowledge << string_of_knowledge
+   # binding.pry
+end
+
+def knowledge
+    @knowledge
+    #binding.pry
+end
+
 
 end
+
